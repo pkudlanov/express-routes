dcdbfdecf46aa50eb530f6a654bf59d13573f9ec
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-11 16:57:27 -0700

    post, create item test

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('items routes', () => {
+    it('creates and item with POST', () => {
+        return request(app)
+            .post('/api/v1/items')
+            .send({ name: 'PITA', type: 'Cross Country', distance: 7, painScale: 8 })
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'PITA',
+                    type: 'Cross Country',
+                    distance: 7,
+                    painScale: 8
+                });
+            });
+    });
+});
