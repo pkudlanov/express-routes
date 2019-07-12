cdbb2b19e3d4b47f691c888456e3d47addee86d2
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-11 17:23:39 -0700

    get, test returns array of all trails with get

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('items routes', () => {
-    it('creates and item with POST', () => {
+    it('creates a trail with POST', () => {
         return request(app)
             .post('/api/v1/trails')
             .send({ name: 'PITA', type: 'Cross Country', distance: 7, painScale: 8 })
@@ -13,6 +13,19 @@ describe('items routes', () => {
                     distance: 7,
                     painScale: 8
                 });
+            });
+    });
+    
+    it('returns the whole array of trails with GET', () => {
+        return request(app)
+            .get('./api/v1/trails')
+            .then(res => {
+                expect(res.body).toEqual([{
+                    name: 'PITA',
+                    type: 'Cross Country',
+                    distance: 7,
+                    painScale: 8
+                }]);
             });
     });
 });
