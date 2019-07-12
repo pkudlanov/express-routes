afc242ed0d5c226ed4fb73c8369ac46f01df04d3
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-11 17:11:54 -0700

    implemented post and its passing test

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ const app = require('../lib/app');
 describe('items routes', () => {
     it('creates and item with POST', () => {
         return request(app)
-            .post('/api/v1/items')
+            .post('/api/v1/trails')
             .send({ name: 'PITA', type: 'Cross Country', distance: 7, painScale: 8 })
             .then(res => {
                 expect(res.body).toEqual({
